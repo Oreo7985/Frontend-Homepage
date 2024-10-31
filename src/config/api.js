14b3450eab8afdbd5459eb2593f988f2a0d9d683
Luhang Fang <luhang.fang@student.hu-berlin.de>
2024-10-31 22:25:01 +0100

    add render url to api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://oreo7985.github.io/Frontend-Homepage'  // 生产环境后端地址
+    ? 'https://frontend-homepage.onrender.com'  // Render后端地址
     : 'http://localhost:5002';           // 开发环境后端地址
 
 export const INSTAGRAM_API_URL = `${API_BASE_URL}/api/instagram`;
