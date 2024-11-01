65df5017d1b44200f125d50def0e0f4f46204787
Luhang Fang <luhang.fang@student.hu-berlin.de>
2024-11-01 14:29:16 +0100

    Add Cloudflare Pages configuration files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Frontend-Homepage',
+  // base: '/Frontend-Homepage',
+  base: '/', 
   plugins: [react()],
   server:{
     port:5001
