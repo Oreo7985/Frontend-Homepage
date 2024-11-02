18a05ae538f1daec83dc01516b209742a7e24274
Luhang Fang <luhang.fang@student.hu-berlin.de>
2024-11-02 21:20:22 +0100

    hashrouter removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -8,28 +8,26 @@ import Instagram from './pages/Instagram'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
-          {/* 移除路径中的前导斜杠 */}
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="cssexercise" element={<CSSExercise />} /> {/* 统一小写 */}
+          <Route path="cssexercise" element={<CSSExercise />} />
           <Route path="instagram" element={<Instagram/>} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
-// 404 组件
 function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh]">
       <h1 className="text-4xl font-bold mb-4">404</h1>
-      <p className="text-gray-600 mb-4">Page not found</p>
+      <p className="text-gray-600 dark:text-gray-400 mb-4">Page not found</p>
       <Link to="/" className="text-blue-500 hover:underline">
         Go back home
       </Link>
