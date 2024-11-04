2add000663a70adbf6d09d68b33016791b0fee40
Luhang Fang <luhang.fang@student.hu-berlin.de>
2024-11-04 22:32:05 +0100

    small changes adapt to api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // src/config/api.js
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
     // ? 'https://frontend-homepage.onrender.com'  // Render后端地址
-    ? 'https://instagram-api.luhang-fang.workers.dev/'  // Cloudflare Workers地址
-    : 'http://localhost:5002';           // 开发环境后端地址
+    ? 'https://instagram-api.luhang-fang.workers.dev'  // Cloudflare Workers地址
+    : 'http://localhost:8787';           // 开发环境后端地址
 
 export const INSTAGRAM_API_URL = `${API_BASE_URL}/api/instagram`;
